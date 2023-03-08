54767d5375601327431bbb25c4806d44cf0b6cf8
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-03-08 18:10:27 +0000

    css errors

--- js/script.js
+++ js/script.js
@@ -4,13 +4,9 @@
 // Created on: March 8 2023
 // This file contains the JS functions for index.html
 
-function doMathClicked () {
+function doPandAClicke() {
 
   // this function does basic math
-  document.getElementById('add-math').innerHTML = '6 + 5 = ' + (6 + 5)
-  document.getElementById('subtract-math').innerHTML = '7 - 3 =  ' + (7 - 3)
-  document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
-  document.getElementById('divide-math').innerHTML = '(4 / 2) + 3 = ' + ((4 / 2) + 3)
-  document.getElementById('exponent-math').innerHTML = '5 + 2 ** 3 = ' + (5 + 2 ** 3)
-  document.getElementById('square-root-math').innerHTML = '√(81) = ' + Math.sqrt(81)
+  document.getElementById('perimeter-math').innerHTML = 'The perimeter of the cottage is ' + (20 + 40) + "cm<sup>2</sup>"
+  document.getElementById('area-math').innerHTML = 'The area of the cottage is ' + (20 * 40) + "cm<sup>2</sup>"
 }
